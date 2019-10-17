44832d9b1574a51507021c3ed00b116c06a32f20
Antonio Gallimore <antonio.gallimore@gmail.com>
2019-10-17 22:17:37 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,17 @@
 class Student < User
 
-def initialize(knowledge)
-  @knowledge = []
-end 
 
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(knowledge)
+    @knowledge << knowledge
+  end
+
+  def knowledge
+    @knowledge
+  end
 
 
 
